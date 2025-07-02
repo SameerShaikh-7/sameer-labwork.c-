9d1835c81919f67a7c8d8404d97f035df7fe8205
SameerShaikh-7 <158576973+tankkhushal@users.noreply.github.com>
2025-07-02 14:17:36 +0530

    HELLO

---
+++ 1.1/basic.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    cout <<"Name   : Sameer\n";
+    cout <<"Age    : 18\n";
+    cout <<"Course : Full Stack\n\n";
+
+
+
+    cout <<"--------\n";
+    cout <<"|\t| \n";
+    cout <<"R\t| \n";
+    cout <<"N\t| \n";
+    cout <<"W\t| \n";
+    cout <<"|\t| \n";
+    cout <<"--------\n\n";
+
+    cout <<"*\n";
+    cout <<"* *\n";
+    cout <<"* * *\n";
+    cout <<"* *\n";
+    cout <<"*\n\n";
+
+    cout <<"*\n";
+    cout <<"*\n";
+    cout <<"*\n";
+    cout <<"*  * *\t*\n";
+
+
+
+}
