48223904fc7583618af337ed298db1dde4277f36
SameerShaikh-7 <158576973+tankkhushal@users.noreply.github.com>
2025-07-22 12:29:10 +0530

    GITHUB

--- 1.1/basic.cpp
+++ 1.1/basic.cpp
@@ -3,45 +3,39 @@ using namespace std;
 
 main()
 {
-    cout <<"Name   : Sameer\n";
-    cout <<"Age    : 18\n";
-    cout <<"Course : Full Stack\n\n";
+    cout << "Name   : Sameer\n";
+    cout << "Age    : 18\n";
+    cout << "Course : Full Stack \n\n";
 
+    cout << "--------\n";
+    cout << "|\t| \n";
+    cout << "R\t| \n";
+    cout << "N\t| \n";
+    cout << "W\t| \n";
+    cout << "|\t| \n";
+    cout << "--------\n\n";
 
+    cout << "*\n";
+    cout << "* *\n";
+    cout << "* * *\n";
+    cout << "* *\n";
+    cout << "*\n\n";
 
-    cout <<"--------\n";
-    cout <<"|\t| \n";
-    cout <<"R\t| \n";
-    cout <<"N\t| \n";
-    cout <<"W\t| \n";
-    cout <<"|\t| \n";
-    cout <<"--------\n\n";
+    cout << "*\n";
+    cout << "*\n";
+    cout << "*\n";
+    cout << "*          * *             *\n";
+    cout << "*        *     *         *\n";
+    cout << "*      *         *     *\n";
+    cout << "*    *             * *\n";
+    cout << "*  *\n";
+    cout << "*\n\n";
 
-    cout <<"*\n";
-    cout <<"* *\n";
-    cout <<"* * *\n";
-    cout <<"* *\n";
-    cout <<"*\n\n";
-
-    cout <<"*\n";
-    cout <<"*\n";
-    cout <<"*\n";
-    cout <<"*          * *             *\n";
-    cout <<"*        *     *         *\n";
-    cout <<"*      *         *     *\n";
-    cout <<"*    *             * *\n";
-    cout <<"*  *\n";
-    cout <<"*\n\n";
-
-    cout <<"  *  *  *         *      *       *          *\n";
-    cout <<"*         *       *      *       *          *\n";
-    cout <<"*       *         * *    *       *          *\n";
-    cout <<"*  *  *           *   *  *       *          *\n";
-    cout <<"*      *          *    * *       *    *     *\n";
-    cout <<"*       *         *      *       *  *   *   *\n";
-    cout <<"*        *        *      *       *          *\n";
-
-
-
-
+    cout << "  *  *  *         *      *       *          *\n";
+    cout << "*         *       *      *       *          *\n";
+    cout << "*       *         * *    *       *          *\n";
+    cout << "*  *  *           *   *  *       *          *\n";
+    cout << "*      *          *    * *       *    *     *\n";
+    cout << "*       *         *      *       *  *   *   *\n";
+    cout << "*        *        *      *       *          *\n";
 }
---
+++ 2-1/Q2-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace  std;
+
+
+main()
+{
+    int a = 12, b = 6;
+    cout<<endl;
+    cout<< a <<" + "<< b <<" = "<< a + b <<endl;
+    cout<< a <<" - "<< b <<" = "<< a - b <<endl;
+    cout<< a <<" * "<< b <<" = "<< a * b <<endl;
+    cout<< a <<" / "<< b <<" = "<< a / b <<endl;
+    cout<< a <<" % "<< b <<" = "<< a % b <<endl;
+
+
+}
