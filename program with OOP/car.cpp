f47e18f84bbf685a79da0240fcbd729e62633cc9
SameerShaikh-7 <shaikh.sameer.76220@gmail.com>
2025-08-26 13:46:12 +0530

    Add files via upload

---
+++ program with OOP/car.cpp
@@ -0,0 +1,211 @@
+#include <iostream>
+using namespace std;
+
+
+class Car{
+
+    public:
+        int car_id;
+        char car_company_name[20];
+        char car_color[10];
+        char car_model[10];
+        int car_release_year;
+
+};
+
+
+int main()
+{
+
+// Input 
+
+
+// Car 1 
+
+   Car car1;
+
+   cout <<endl << "Car 1 " << endl;
+
+   cout << endl << "Enter car id : ";
+   cin >> car1.car_id;
+
+   cout << endl << "Enter Company Name : ";
+   cin >> car1.car_company_name;
+
+   cout << endl << "Enter Colour : ";
+   cin >> car1.car_color;
+   
+   cout << endl << "Enter Model : ";
+   cin >> car1.car_model;
+
+   cout << endl << "Enter Release Year : ";
+   cin >> car1.car_release_year;
+
+
+
+
+// Car 2
+
+   Car car2;
+
+   cout <<endl << "Car 2 " << endl;
+   
+   cout << endl << "Enter car id : ";
+   cin >> car2.car_id;
+
+   cout << endl << "Enter Company Name : ";
+   cin >> car2.car_company_name;
+
+   cout << endl << "Enter Colour : ";
+   cin >> car2.car_color;
+   
+   cout << endl << "Enter Model : ";
+   cin >> car2.car_model;
+
+   cout << endl << "Enter Release Year : ";
+   cin >> car2.car_release_year;
+
+
+
+// Car 3
+
+   Car car3;
+
+   cout <<endl << "Car 3 " << endl;
+   
+   cout << endl << "Enter car id : ";
+   cin >> car3.car_id;
+
+   cout << endl << "Enter Company Name : ";
+   cin >> car3.car_company_name;
+
+   cout << endl << "Enter Colour : ";
+   cin >> car3.car_color;
+   
+   cout << endl << "Enter Model : ";
+   cin >> car3.car_model;
+
+   cout << endl << "Enter Release Year : ";
+   cin >> car3.car_release_year;
+
+
+
+// Car 4
+
+   Car car4;
+
+   cout <<endl << "Car 4 " << endl;
+   
+   cout << endl << "Enter car id : ";
+   cin >> car4.car_id;
+
+   cout << endl << "Enter Company Name : ";
+   cin >> car4.car_company_name;
+
+   cout << endl << "Enter Colour : ";
+   cin >> car4.car_color;
+   
+   cout << endl << "Enter Model : ";
+   cin >> car4.car_model;
+
+   cout << endl << "Enter Release Year : ";
+   cin >> car4.car_release_year;
+
+
+
+// Output 
+
+cout << endl <<"----Car Details----" <<endl;
+
+// Car 1 Output 
+
+cout << endl << "Car 1 " << endl;
+
+cout << "id\t : ";
+cout << car1.car_id << endl;
+
+cout << "Company\t : ";
+cout << car1.car_company_name << endl;
+
+cout << "Color\t : ";
+cout << car1.car_color << endl;
+
+cout << "Model\t : ";
+cout << car1.car_model<< endl;
+
+cout << "Year\t : ";
+cout << car1.car_release_year<< endl;
+
+
+// Car 2 Output  
+
+cout << endl << "Car 2 " << endl;
+
+cout << "id\t : ";
+cout << car2.car_id << endl;
+
+cout << "Company\t : ";
+cout << car2.car_company_name << endl;
+
+cout << "Color\t : ";
+cout << car2.car_color << endl;
+
+cout << "Model\t : ";
+cout << car2.car_model<< endl;
+
+cout << "Year\t : ";
+cout << car2.car_release_year<< endl;
+
+
+
+// Car 3 Output  
+
+cout << endl << "Car 3 " << endl;
+
+cout << "id\t : ";
+cout << car3.car_id << endl;
+
+cout << "Company\t : ";
+cout << car3.car_company_name << endl;
+
+cout << "Color\t : ";
+cout << car3.car_color << endl;
+
+cout << "Model\t : ";
+cout << car3.car_model<< endl;
+
+cout << "Year\t : ";
+cout << car3.car_release_year<< endl;
+
+
+
+// Car 4 Output  
+
+cout << endl << "Car 4 " << endl;
+
+cout << "id\t : ";
+cout << car4.car_id << endl;
+
+cout << "Company\t : ";
+cout << car4.car_company_name << endl;
+
+cout << "Color\t : ";
+cout << car4.car_color << endl;
+
+cout << "Model\t : ";
+cout << car4.car_model<< endl;
+
+cout << "Year\t : ";
+cout << car4.car_release_year<< endl;
+
+
+
+
+
+   
+   
+ 
+
+
+
+}
---
+++ program with OOP/employee.cpp
@@ -0,0 +1,333 @@
+#include <iostream>
+using namespace std;
+
+// classes 
+class Employee 
+{
+
+public :
+    int emp_id ;
+    char emp_name[20];
+    int emp_age;
+    char emp_role[20];
+    int emp_salary;
+    char emp_city[15];
+    int emp_experience;
+    char emp_company_name[20];
+
+};
+
+
+
+int main ()
+{
+
+// Input 
+
+ Employee employee;
+
+cout << endl << "Employee 1" << endl;
+cout <<endl << "Enter your id : ";
+cin >> employee.emp_id;
+
+cout <<endl << "Enter your name : ";
+cin >> employee.emp_name;
+
+cout <<endl << "Enter your age : ";
+cin >> employee.emp_age;
+
+cout <<endl << "Enter your role : ";
+cin >> employee.emp_role;
+
+cout <<endl << "Enter your salary : ";
+cin >> employee.emp_salary;
+
+cout <<endl << "Enter your city : ";
+cin >> employee.emp_city;
+
+cout <<endl << "Enter your experience : ";
+cin >> employee.emp_experience;
+
+cout <<endl << "Enter your company_name : ";
+cin >> employee.emp_company_name;
+
+
+// Employe2
+
+Employee employee2;
+
+cout << endl << "Employee 2" << endl;
+cout <<endl << "Enter your id : ";
+cin >> employee2.emp_id;
+
+cout <<endl << "Enter your name : ";
+cin >> employee2.emp_name;
+
+cout <<endl << "Enter your age : ";
+cin >> employee2.emp_age;
+
+cout <<endl << "Enter your role : ";
+cin >> employee2.emp_role;
+
+cout <<endl << "Enter your salary : ";
+cin >> employee2.emp_salary;
+
+cout <<endl << "Enter your city : ";
+cin >> employee2.emp_city;
+
+cout <<endl << "Enter your experience : ";
+cin >> employee2.emp_experience;
+
+cout <<endl << "Enter your company_name : ";
+cin >> employee2.emp_company_name;
+
+// Employe3
+
+Employee employee3;
+
+cout << endl << "Employee 3" << endl;
+
+cout <<endl << "Enter your id : ";
+cin >> employee3.emp_id;
+
+cout <<endl << "Enter your name : ";
+cin >> employee3.emp_name;
+
+cout <<endl << "Enter your age : ";
+cin >> employee3.emp_age;
+
+cout <<endl << "Enter your role : ";
+cin >> employee3.emp_role;
+
+cout <<endl << "Enter your salary : ";
+cin >> employee3.emp_salary;
+
+cout <<endl << "Enter your city : ";
+cin >> employee3.emp_city;
+
+cout <<endl << "Enter your experience : ";
+cin >> employee3.emp_experience;
+
+cout <<endl << "Enter your company_name : ";
+cin >> employee3.emp_company_name;
+
+// Empoyee 4
+
+Employee employee4;
+
+cout << endl << "Employee 4" << endl;
+cout <<endl << "Enter your id : ";
+cin >> employee4.emp_id;
+
+cout <<endl << "Enter your name : ";
+cin >> employee4.emp_name;
+
+cout <<endl << "Enter your age : ";
+cin >> employee4.emp_age;
+
+cout <<endl << "Enter your role : ";
+cin >> employee4.emp_role;
+
+cout <<endl << "Enter your salary : ";
+cin >> employee4.emp_salary;
+
+cout <<endl << "Enter your city : ";
+cin >> employee4.emp_city;
+
+cout <<endl << "Enter your experience : ";
+cin >> employee4.emp_experience;
+
+cout <<endl << "Enter your company_name : ";
+cin >> employee4.emp_company_name;
+
+// Empoyee 5
+
+Employee employee5;
+
+cout << endl << "Employee 5" << endl;
+cout <<endl << "Enter your id : ";
+cin >> employee5.emp_id;
+
+cout <<endl << "Enter your name : ";
+cin >> employee5.emp_name;
+
+cout <<endl << "Enter your age : ";
+cin >> employee5.emp_age;
+
+cout <<endl << "Enter your role : ";
+cin >> employee5.emp_role;
+
+cout <<endl << "Enter your salary : ";
+cin >> employee5.emp_salary;
+
+cout <<endl << "Enter your city : ";
+cin >> employee5.emp_city;
+
+cout <<endl << "Enter your experience : ";
+cin >> employee5.emp_experience;
+
+cout <<endl << "Enter your company_name : ";
+cin >> employee5.emp_company_name;
+
+
+// Output
+
+cout << endl << "All employee detail----" <<endl;
+
+
+// Employee 1 
+
+cout << endl << "Employee 1 "<<endl;
+
+cout <<endl << "id\t : ";
+cout << employee.emp_id <<endl;
+
+cout <<endl << "Name\t : ";
+cout << employee.emp_name <<endl;
+
+cout <<endl << "Age\t : ";
+cout << employee.emp_age <<endl;
+
+cout <<endl << "Role\t : ";
+cout << employee.emp_role <<endl;
+
+cout <<endl << "Salary\t : ";
+cout << employee.emp_salary <<endl;
+
+cout <<endl << "City\t : ";
+cout << employee.emp_city <<endl;
+
+cout <<endl << "Experience\t : ";
+cout << employee.emp_experience <<endl;
+
+cout <<endl << "Company name\t : ";
+cout << employee.emp_company_name <<endl;
+
+// Employee 2
+
+cout << endl << "Employee 2 "<<endl;
+
+cout <<endl << "id\t : ";
+cout << employee2.emp_id <<endl;
+
+cout <<endl << "Name\t : ";
+cout << employee2.emp_name <<endl;
+
+cout <<endl << "Age\t : ";
+cout << employee2.emp_age <<endl;
+
+cout <<endl << "Role\t : ";
+cout << employee2.emp_role <<endl;
+
+cout <<endl << "Salary\t : ";
+cout << employee2.emp_salary <<endl;
+
+cout <<endl << "City\t : ";
+cout << employee2.emp_city <<endl;
+
+cout <<endl << "Experience\t : ";
+cout << employee2.emp_experience <<endl;
+
+cout <<endl << "Company name\t : ";
+cout << employee2.emp_company_name <<endl;
+
+// Employee 3
+
+cout << endl << "Employee 3 "<<endl;
+
+cout <<endl << "id\t : ";
+cout << employee3.emp_id <<endl;
+
+cout <<endl << "Name\t : ";
+cout << employee3.emp_name <<endl;
+
+cout <<endl << "Age\t : ";
+cout << employee3.emp_age <<endl;
+
+cout <<endl << "Role\t : ";
+cout << employee3.emp_role <<endl;
+
+cout <<endl << "Salary\t : ";
+cout << employee3.emp_salary <<endl;
+
+cout <<endl << "City\t : ";
+cout << employee3.emp_city <<endl;
+
+cout <<endl << "Experience\t : ";
+cout << employee3.emp_experience <<endl;
+
+cout <<endl << "Company name\t : ";
+cout << employee3.emp_company_name <<endl;
+
+// Employee 4
+
+cout << endl << "Employee 4 "<<endl;
+
+cout <<endl << "id\t : ";
+cout << employee4.emp_id <<endl;
+
+cout <<endl << "Name\t : ";
+cout << employee4.emp_name <<endl;
+
+cout <<endl << "Age\t : ";
+cout << employee4.emp_age <<endl;
+
+cout <<endl << "Role\t : ";
+cout << employee4.emp_role <<endl;
+
+cout <<endl << "Salary\t : ";
+cout << employee4.emp_salary <<endl;
+
+cout <<endl << "City\t : ";
+cout << employee4.emp_city <<endl;
+
+cout <<endl << "Experience\t : ";
+cout << employee4.emp_experience <<endl;
+
+cout <<endl << "Company name\t : ";
+cout << employee4.emp_company_name <<endl;
+
+// Employee 5 
+
+cout << endl << "Employee 5 "<<endl;
+
+cout <<endl << "id\t : ";
+cout << employee5.emp_id <<endl;
+
+cout <<endl << "Name\t : ";
+cout << employee5.emp_name <<endl;
+
+cout <<endl << "Age\t : ";
+cout << employee5.emp_age <<endl;
+
+cout <<endl << "Role\t : ";
+cout << employee5.emp_role <<endl;
+
+cout <<endl << "Salary\t : ";
+cout << employee5.emp_salary <<endl;
+
+cout <<endl << "City\t : ";
+cout << employee5.emp_city <<endl;
+
+cout <<endl << "Experience\t : ";
+cout << employee5.emp_experience <<endl;
+
+cout <<endl << "Company name\t : ";
+cout << employee5.emp_company_name <<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
